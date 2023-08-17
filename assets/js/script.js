d5bbf638718c58adff9bcb2a03fb6a65d47458a4
kritagya20 <kritagya2022@gmail.com>
2023-08-17 23:02:16 +0530

    initial commit of the cosmic website. the website is built over astro pic code with multiple design changes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,10 @@ const defaultQuery = url + apiKey ;
 const searchForm =  document.querySelector('#search-form');
 const errorCard = document.querySelector('#error-dialog');
 const errorCardText = errorCard.querySelector('#error-dialog-text');
-console.log(errorCardText)
 const retryBtn = document.querySelector('#retry');
-const container = document.querySelector('#api-data');
+const container = document.querySelector('#data');
+const searchDate = container.querySelector('#search-result-date');
+const innerContainer = document.querySelector('#api-data-inner');
 const loadingSpinner = document.getElementById('spinner'); //LOADING SPINNER
 let apiNotFound = false;
 
@@ -22,7 +23,7 @@ window.addEventListener('load', () => {
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault(); //TO AVOID THE DEFAULT ACTION ASSOSIATED WITH SUBMIT EVENT IN JS
     SearchWithDate();
-    container.style.display = "none";
+    innerContainer.style.display = "none";
 });
 
 //GETTING DATA FROM HTML 
@@ -46,15 +47,15 @@ const SearchWithDate = ()=>{
             errorCard.showModal();
             clearData();
         } else { 
-            if (yyyy<=Present_year && mm>Present_month){
+            if (yyyy==Present_year && mm>Present_month){
                 errorCardText.innerHTML  =`Invalid Date!!<br/> Try something between <b>01/01/1996</b>  to <b>${Present_date}/${Present_month}/${Present_year}</b>`;
                 errorCard.showModal();
                 clearData();
             } else {
-                if(yyyy<=Present_year && mm<=Present_month && dd>Present_date){
+                if(yyyy==Present_year && mm==Present_month && dd>Present_date){
                     errorCardText.innerHTML  =`Invalid Date!!<br/> Try something between <b>01/01/1996</b>  to <b>${Present_date}/${Present_month}/${Present_year}</b>`;
                     errorCard.showModal();
-                clearData();
+                    clearData();
                 } else {                
                     fetchData(dateQuery);
                     clearData();
@@ -62,9 +63,9 @@ const SearchWithDate = ()=>{
             }
         };
     }
-    validating_dates(); 
+    validating_dates();     
+    searchDate.innerHTML = `<h4>Search Results for: ${dd}/${mm}/${yyyy} </h4>`;
 };
-
 
 
 //CLEARING DATA
@@ -77,17 +78,16 @@ const clearData = () => {
 //FETCHING DATA FROM API
 const fetchData = async(query) => {
     try{
-        loadingSpinner.style.display = "flex";
+        loadingSpinner.style.display = "flex"; //display spinner while fetching data
         let response = await fetch(query); //FETCHING  DATA
         let fetchedData = await response.json(); //CONVERTING  DATA
-        loadingSpinner.style.display = "none";
+        loadingSpinner.style.display = "none";//hide spinner after data fetching
         apiNotFound = false;
-        renderData(fetchedData, apiNotFound);
+        renderData(fetchedData, apiNotFound); //passing the data for display
     } catch(error){
         apiNotFound = true;
-        renderData(fetchData, apiNotFound);
-        loadingSpinner.style.display = "none";
-        console.log(error);
+        renderData(fetchData, apiNotFound);//passing the data for display
+        loadingSpinner.style.display = "none"; //hiding spinner
     }
 }
 
@@ -96,6 +96,7 @@ fetchData(defaultQuery);
 
 //DISPLAYING THE FETCHED DATA IN DOM
 const renderData = (data, apiNotFound) => {
+    //RENDERING DATA ON SCREEN IF DATA IS FETCHED SUCCESSFULLY 
     if(!apiNotFound){
         let copyright = data["copyright"];
         let explanation = data["explanation"];
@@ -103,6 +104,7 @@ const renderData = (data, apiNotFound) => {
         let media_type = data["media_type"];
         let title = data["title"];
         let url = data["url"];
+        console.log(data)
 
         let imageType = ` 
             <a id="hd-image-url" href="" target="_blank">              
@@ -122,8 +124,16 @@ const renderData = (data, apiNotFound) => {
         `;
 
         // Static elements
-        document.getElementById("title").innerHTML = title;
-        document.getElementById("description").innerHTML = explanation;
+        if(data['msg']) {
+            searchDate.innerHTML = `<h4>No Data Found for: ${dd}/${mm}/${yyyy} </h4>`;
+            document.getElementById("media").style.classList = 'nd';
+            document.getElementById("title").innerHTML = '';
+            document.getElementById("description").innerHTML = '';    
+
+        } else{
+            document.getElementById("title").innerHTML = title;
+            document.getElementById("description").innerHTML = explanation;    
+        }
 
         // If statement for images/videos
         if (media_type === "video") {
@@ -145,10 +155,10 @@ const renderData = (data, apiNotFound) => {
         //CLEAR FORM DATA
         clearData();
     }else{        
-        errorCard.showModal();
+        errorCard.showModal(); //displaying error message
     }
     
-    container.style.display = "flex";
+    innerContainer.style.display = "flex";
 }
 
 //RETRY ON CLICK
@@ -156,156 +166,3 @@ retryBtn.addEventListener('click', () => {
     errorCard.close();
     fetchData(defaultQuery);
 });
-
-
-// CANVAS CODE FOR PARTICLE ANIMATION
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d'); // it returns an object containing bunch of properties related to canvas
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-let particlesArray;
-
-//get mouse position 
-let mouse = {
-    x: null,
-    y: null,
-    radius: (canvas.height/80) * (canvas.width/80)
-}
-
-window.addEventListener('mousemove',
-    function(event) {
-        mouse.x = event.x;
-        mouse.y = event.y;
-    }
-);
-
-// create particles
-class Particle {
-    constructor( x, y, directionX, directionY, size, color) {
-       this.x = x;
-       this.y = y;
-       this.directionX = directionX;
-       this.directionY = directionY;
-       this.size = size;
-       this.color = color; 
-    }
-
-    //method to draw individual particles
-    draw() {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-        ctx.fillStyle = 'rgba(255, 204, 18, .8)';
-        ctx.fill();
-    }
-
-    //check particle position, check mouse position, move the partiles, draw tha particles
-    update(){
-        //check if particles is still within canvas
-        if(this.x > canvas.width || this.x < 0 ) {
-            this.directionX = -this.directionX;
-
-        }
-        if( this.y > canvas.height || this.y < 0 ) {
-            this.directionY = -this.directionY;
-        }
-
-        //check collision detection - mouse position / particle position
-        let dx = mouse.x - this.x;
-        let dy = mouse.x - this.y;
-        let distance = Math.sqrt(dx*dx + dy*dy);
-        //pushing away the colliding particles
-        if (distance < mouse.radius + this.size) {
-            //pushing away the colliding particles
-            if(mouse.x < this.x && this.y < canvas.width - this.size * 10) {
-                this.x += 10;
-            }
-            if (mouse.x > this.x && this.x > this.size * 10) {
-                this.x -=10;
-            }
-            if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
-                this.y +=10;
-            }
-            if (mouse.y > this.y && this.y > canvas.height - this.size * 10) {
-                this.y -=10;
-            }
-        }
-        //moving non colliding particles
-        this.x += this.directionX;
-        this.y += this.directionY;
-
-        //draw particles
-        this.draw();
-    }
-}
-
-//create particle array
-function init() {
-    particlesArray = [];
-    let numberOfParticles = (canvas.height * canvas.width) / 9000;
-    for (let i=0; i<numberOfParticles*1; i++) {
-        let size = (Math.random()*3) +1;
-        let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
-        let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
-        let directionX = (Math.random() * 3) - 1.5;
-        let directionY = (Math.random() * 3) - 1.5;
-        let color = 'rgba(255, 204, 18, .8)';
-    
-        particlesArray.push(new Particle(x, y, directionX, directionY, size, color))
-    }
-}
-
-//animation loop 
-function animate(){
-    requestAnimationFrame(animate);
-    ctx.clearRect(0,0,innerWidth,innerHeight);
-
-    for (let i=0; i<particlesArray.length; i++) {
-        particlesArray[i].update();
-    }
-    connect();
-}
-
-// check if particles are close enough to draw line between them
-function connect() {
-    let opacityValue =1;
-    for( let a=0; a<particlesArray.length; a++) {
-        for ( let b=a; b<particlesArray.length; b++) {
-            let distance = ((particlesArray[a].x - particlesArray[b].x) * (particlesArray[a].x - particlesArray[b].x))
-            + ((particlesArray[a].y - particlesArray[b].y) * (particlesArray[a].y - particlesArray[b].y));
-            
-            if (distance < (canvas.width/7) * (canvas.height/7)) {
-                opacityValue = 1 - (distance/20000);
-                ctx.strokeStyle= 'rgba(255, 218, 82,'+ opacityValue + ')';
-                ctx.strokeStyle = 'rgba(255, 218, 82, .75)';
-                ctx.lineWidth = 1;
-                ctx.beginPath();
-                ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
-                ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
-                ctx.stroke();
-            
-            }
-        
-        }
-    }
-}
-
-// resize event 
-window.addEventListener('resize', 
-function() {
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
-    mouse.radius = ((canvas.height/80) * (canvas.height/80));
-});
-
-//mouse out event 
-window.addEventListener('mouseout', 
-function() {
-    mouse.x = undefined;
-    mouse.y = undefined;
-});
-
-init();
-animate();
-
-
-
